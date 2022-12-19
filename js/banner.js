28cfc04fb3e20a2e83530e3bffe569180ee05708
Rocio Morales <roncio.93@gmail.com>
2022-12-19 20:56:56 -0300

    :art: Add style to banner.

---
+++ js/banner.js
@@ -0,0 +1,17 @@
+const bannerImages = [
+    {
+        'imgUrl': 'https://www.fullh4rd.com.ar/adminrgb/img/banner/74.png'
+    },
+    {
+        'imgUrl': 'https://www.fullh4rd.com.ar/adminrgb/img/banner/117.png'
+    },
+    {
+        'imgUrl': 'https://www.fullh4rd.com.ar/adminrgb/img/banner/126.png'
+    },
+    {
+        'imgUrl': 'https://www.fullh4rd.com.ar/adminrgb/img/banner/135.png'
+    },
+    {
+        'imgUrl': 'https://www.fullh4rd.com.ar/adminrgb/img/banner/136.png'
+    }
+]
